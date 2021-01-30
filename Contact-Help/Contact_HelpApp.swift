6b23006648c00f04853dcdba158830b70f28509e
PRABAL4546 <prabaljitwalia4546@gmail.com>
2021-01-30 11:06:21 +0530

    first commit

---
+++ Contact-Help/Contact_HelpApp.swift
@@ -0,0 +1,17 @@
+//
+//  Contact_HelpApp.swift
+//  Contact-Help
+//
+//  Created by PRABALJIT WALIA     on 30/01/21.
+//
+
+import SwiftUI
+
+@main
+struct Contact_HelpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Contact-Help/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Contact-Help
+//
+//  Created by PRABALJIT WALIA     on 30/01/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Button("Import Picture"){
+            
+}
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
